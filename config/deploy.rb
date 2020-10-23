d1f94a8fb60d1654e5359a43dce04a30f31d87a7
zhangjingwei33 <zhangjingwei33@gmail.com>
2020-10-23 23:50:44 +0800

    change URL adress

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.11.0"
 
 set :application, "lmzdx-demo"
-set :repo_url, "git@github.com:zhangjingwei127/lmzdx-demo.git"
+set :repo_url, "git@github.com:zhangjingwei33/lmzdx-demo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
